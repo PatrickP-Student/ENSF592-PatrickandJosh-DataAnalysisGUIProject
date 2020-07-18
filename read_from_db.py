c5f9ee2c4751599720de53be87e36188cdfb2df4
Patrick Pickard <pmpickar@ualberta.ca>
2020-07-17 18:45:05 -0600

    Completed write_to_db.py, changed data structures of data to nested lists, and wrote data to the MongoDB Atlas db

---
+++ read_from_db.py
@@ -0,0 +1,7 @@
+'''
+Filename: read_from_db.py
+Author: Patrick Pickard
+Date: July 17, 2020 - Friday
+Description: This script will read 6 list objects (with nested lists in elements) from the database,
+and perform all processing, sorting, and handling required for use in our program.
+'''
--- write_to_db.py
+++ write_to_db.py
@@ -4,8 +4,8 @@ Author: Patrick Pickard
 Date: July 17, 2020 - Friday
 Description: This script will read 6 .csv files from the local repository location into
 panda objects for formatting, perform additional formatting on the data read (removing unneeded 
-data from any data sets, ordering columns, etc), convert the data structure to a dict() type, and 
-write the dictionaries to the database specified
+data from any data sets, ordering columns, etc), convert the data structure to a list[] type, and 
+write the lists to the database specified
 '''
 
 import pymongo
@@ -31,7 +31,10 @@ df1 = pd.read_csv("Traffic_Incidents_Archive_2016.csv")
 # formats the order of the columns
 df1 = df1[['INCIDENT INFO', 'DESCRIPTION', 'START_DT', 'MODIFIED_DT', 'QUADRANT', 'Longitude', 'Latitude', 'location', 'Count']] 
 # converts the data structure to a dictionary
-records1 = df1.to_dict(orient = 'dict')
+records1 = df1.to_dict(orient = 'list')
+
+# writes the dictionaries holding the data into the database
+collection1.insert_one(records1) ## THIS WILL UPLOAD THE DICTS TO THE DB
 
 # this above code will be repeated (in general template format) for all files to be written to the
 # database. Subsequent files code blocks are shown below.
@@ -40,7 +43,9 @@ records1 = df1.to_dict(orient = 'dict')
 collection2 = db["CityofCalgary - Traffic Incidents 2"]
 df2 = pd.read_csv("Traffic_Incidents_Archive_2017.csv")
 df2 = df2[['INCIDENT INFO', 'DESCRIPTION', 'START_DT', 'MODIFIED_DT', 'QUADRANT', 'Longitude', 'Latitude', 'location', 'Count']]
-records2 = df2.to_dict(orient = 'dict')
+records2 = df2.to_dict(orient = 'list')
+
+collection2.insert_one(records2)
 
 ## this is for the 2018 data file
 collection3 = db["CityofCalgary - Traffic Incidents 3"]
@@ -54,18 +59,15 @@ df3 = df3[~df3.START_DT.str.contains("/2016 ")]
 df3 = df3[~df3.START_DT.str.contains("/2017 ")]
 df3 = df3[~df3.START_DT.str.contains("/2019 ")]
 df3 = df3[~df3.START_DT.str.contains("/2020 ")]
-records3 = df3.to_dict(orient = 'dict')
+records3 = df3.to_dict(orient = 'list')
 
 ###### THIS IS FOR TESTING TO SEE IF ALL NON 2018 ROWS HAVE BEEN REMOVED ######
 # for column_header, data in records3.items():
 #     print(column_header)
 #     for key in data:
-#         print(key, data[key])
+#         print(key)
 
-# writes the dictionaries holding the data into the database
-# collection1.insert_many(records1) ## THIS WILL UPLOAD THE DICTS TO THE DB
-# collection2.insert_many(records2)
-# collection3.insert_many(records3)
+collection3.insert_one(records3)
 
 #####################################################################################################
 
@@ -77,20 +79,22 @@ Traffic Volumes - 2016 to 2018
 collection1a = db["CityofCalgary - Traffic Volumes"]
 df1a = pd.read_csv("TrafficFlow2016_OpenData.csv") 
 df1a = df1a[['secname', 'the_geom', 'year_vol', 'shape_leng', 'volume']]   
-records1a = df1a.to_dict(orient = 'dict')
+records1a = df1a.to_dict(orient = 'list')
+
+collection1a.insert_one(records1a) ## THIS WILL UPLOAD THE DICTS TO THE DB
 
 ## this is for the 2017 data file
 collection2a = db["CityofCalgary - Traffic Volumes 2"]
 df2a = pd.read_csv("2017_Traffic_Volume_Flow.csv") 
 df2a = df2a[['segment_name', 'the_geom', 'year', 'length_m', 'volume']]   
-records2a = df2a.to_dict(orient = 'dict')
+records2a = df2a.to_dict(orient = 'list')
+
+collection2a.insert_one(records2a)
 
 ## this is for the 2018 data file
 collection3a = db["CityofCalgary - Traffic Volumes 3"]
 df3a = pd.read_csv("Traffic_Volumes_for_2018.csv") 
 df3a = df3a[['SECNAME', 'multilinestring', 'YEAR', 'Shape_Leng', 'VOLUME']] 
-records3a = df3a.to_dict(orient = 'dict')
+records3a = df3a.to_dict(orient = 'list')
 
-# collection1a.insert_many(records1a) ## THIS WILL UPLOAD THE DICTS TO THE DB
-# collection2a.insert_many(records2a)
-# collection3a.insert_many(records3a)
+collection3a.insert_one(records3a)
