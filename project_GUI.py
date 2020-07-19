8a39905c6880f20a0e95048d2efa20e69a7167cf
joshua_posyluzny <64817236+jposyluzny@users.noreply.github.com>
2020-07-18 18:54:38 -0600

    tried to get the header formatting issue fixed, no success

--- project_GUI.py
+++ project_GUI.py
@@ -8,7 +8,6 @@ from read_from_db import *
 
 
 def data_types():
-
     if type_combo.get() == "Traffic Incidents" and year_combo.get() == "2016":
         collection_name = "CityofCalgary - Traffic Incidents"
         return collection_name
@@ -28,148 +27,146 @@ def data_types():
         collection_name = "CityofCalgary - Traffic Volumes 3"
         return collection_name
 
-def tree_type():
 
-    if type_combo.get() == "Traffic Volume":
-        tree1.grid(row = 0, column = 1, sticky = "nwes")
-    elif type_combo.get() == "Traffic Incidents":
-        tree2.grid(row = 0, column = 1, sticky = "nwes")
+def tree_type():
+    if type_combo.get() == "Traffic Volumes":
+        tree1.grid(row=0, column=1, sticky="nwes")
+        print(str(data_types()))
+    else:
+        tree2.grid(row=0, column=1, sticky="nwes")
+        print(str(data_types()))
+
+
 
 window = tk.Tk()
 window.title("Traffic Analysis")
-window.columnconfigure([0,1], weight = 1)
-window.rowconfigure(0, weight = 1)
+window.columnconfigure([0, 1], weight=1)
+window.rowconfigure(0, weight=1)
 
-frame_left = tk.Frame(master = window, width = 250, height = 400, bg = "gray55")     # build the left frame that will hold all the buttons
-frame_left.grid(column = 0, sticky = "nsew")
+frame_left = tk.Frame(master=window, width=250, height=400,
+                      bg="gray55")  # build the left frame that will hold all the buttons
+frame_left.columnconfigure(0, weight=1)
+frame_left.rowconfigure([0,7], weight=1)
+frame_left.grid(column=0, sticky="nsew")
 
-frame_right = tk.Frame(master = window, width = 750)       # build the right frame that will hold all the data
-frame_right.grid(column = 1)
-
+frame_right = tk.Frame(master=window, width=1000)  # build the right frame that will hold all the data
+frame_right.columnconfigure(1, weight=1)
+frame_right.rowconfigure(0, weight=1)
+frame_right.grid(column=1)
 
 type_combo = ttk.Combobox(
-    values = ["Traffic Volume", "Traffic Incidents"],      # Data type combobox
-    justify = "center",
-    master = frame_left,
-    width = 16
+    values=["Traffic Volume", "Traffic Incidents"],  # Data type combobox
+    justify="center",
+    master=frame_left,
+    width=16
 )
-type_combo.grid(row = 0, column = 0, padx = 5, pady = 5)
+type_combo.grid(row=0, column=0, padx=5, pady=5)
 
-
-year_combo = ttk.Combobox(                      # Year combobox
-    values = ["2016", "2017", "2018"],
-    justify = "center",
-    master = frame_left,
-    width = 16
+year_combo = ttk.Combobox(  # Year combobox
+    values=["2016", "2017", "2018"],
+    justify="center",
+    master=frame_left,
+    width=16
 )
-year_combo.grid(row = 1, column = 0, padx = 5, pady = 5)
+year_combo.grid(row=1, column=0, padx=5, pady=5)
 
-
-read_btn = tk.Button(                         # Build Read button
-    relief = "solid",
-    master = frame_left,
-    text = "Read",
-    width = 16,
-    command = lambda:[data_types(), tree_type()]
+read_btn = tk.Button(  # Build Read button
+    relief="solid",
+    master=frame_left,
+    text="Read",
+    width=16,
+    command=lambda: [data_types(), tree_type()]
 )
-read_btn.grid(row = 2, column = 0, padx = 5, pady = 5)
+read_btn.grid(row=2, column=0, padx=5, pady=5)
 
 ### TREE1 IS FOR TRAFFIC VOLUME
-#Getting Treeview list
+# Getting Treeview list for Traffic Volume
 tree1 = ttk.Treeview(window)
 
 # Creating Columns
 tree1["columns"] = ("the_geom", "year_vol", "shape_leng", "volume")
 
 # Formatting Columns
-tree1.column("#0", width = 100, minwidth = 50)
-tree1.column("the_geom", width = 100, minwidth = 50)
-tree1.column("year_vol", width = 100, minwidth = 50)
-tree1.column("shape_leng", width = 100, minwidth = 50)
-tree1.column("volume", width = 100, minwidth = 50)
+tree1.column("#0", width=80, minwidth=50)
+tree1.column("the_geom", width=80, minwidth=50)
+tree1.column("year_vol", width=80, minwidth=50)
+tree1.column("shape_leng", width=80, minwidth=50)
+tree1.column("volume", width=80, minwidth=50)
 
-tree1.heading("#0", text = "secname", anchor = tk.W)
-tree1.heading("the_geom", text = "the_geom", anchor = tk.W)
-tree1.heading("year_vol", text = "year_vol", anchor = tk.W)
-tree1.heading("shape_leng", text = "shape_leng", anchor = tk.W)
-tree1.heading("volume", text = "volume", anchor = tk.W)
-
+tree1.heading("#0", text="secname", anchor=tk.W)
+tree1.heading("the_geom", text="the_geom", anchor=tk.W)
+tree1.heading("year_vol", text="year_vol", anchor=tk.W)
+tree1.heading("shape_leng", text="shape_leng", anchor=tk.W)
+tree1.heading("volume", text="volume", anchor=tk.W)
 
 ### TREE2 IS FOR TRAFFIC INCIDENTS
-#Getting Treeview list
+# Getting Treeview list For Traffic Incidents
 tree2 = ttk.Treeview(window)
 
 # Creating Columns
 tree2["columns"] = ("DESCRIPTION", "START_DT", "MODIFIED_DT", "QUADRANT", "Longitude", "Latitude", "location", "Count")
 
 # Formatting Columns
-tree2.column("#0", width = 100, minwidth = 50)
-tree2.column("DESCRIPTION", width = 100, minwidth = 50)
-tree2.column("START_DT", width = 100, minwidth = 50)
-tree2.column("MODIFIED_DT", width = 100, minwidth = 50)
-tree2.column("QUADRANT", width = 100, minwidth = 50)
-tree2.column("Longitude", width = 100, minwidth = 50)
-tree2.column("Latitude", width = 100, minwidth = 50)
-tree2.column("location", width = 100, minwidth = 50)
-tree2.column("Count", width = 100, minwidth = 50)
+tree2.column("#0", width=80, minwidth=50)
+tree2.column("DESCRIPTION", width=80, minwidth=50)
+tree2.column("START_DT", width=80, minwidth=50)
+tree2.column("MODIFIED_DT", width=80, minwidth=50)
+tree2.column("QUADRANT", width=80, minwidth=50)
+tree2.column("Longitude", width=80, minwidth=50)
+tree2.column("Latitude", width=80, minwidth=50)
+tree2.column("location", width=80, minwidth=50)
+tree2.column("Count", width=80, minwidth=50)
 
-tree2.heading("#0", text = "INCIDENT INFO", anchor = tk.W)
-tree2.heading("DESCRIPTION", text = "DESCRIPTION", anchor = tk.W)
-tree2.heading("START_DT", text = "START_DT", anchor = tk.W)
-tree2.heading("MODIFIED_DT", text = "MODIFIED_DT", anchor = tk.W)
-tree2.heading("QUADRANT", text = "QUADRANT", anchor = tk.W)
-tree2.heading("Longitude", text = "Longitude", anchor = tk.W)
-tree2.heading("Latitude", text = "Latitude", anchor = tk.W)
-tree2.heading("location", text = "location", anchor = tk.W)
-tree2.heading("Count", text = "Count", anchor = tk.W)
+tree2.heading("#0", text="INCIDENT INFO", anchor=tk.W)
+tree2.heading("DESCRIPTION", text="DESCRIPTION", anchor=tk.W)
+tree2.heading("START_DT", text="START_DT", anchor=tk.W)
+tree2.heading("MODIFIED_DT", text="MODIFIED_DT", anchor=tk.W)
+tree2.heading("QUADRANT", text="QUADRANT", anchor=tk.W)
+tree2.heading("Longitude", text="Longitude", anchor=tk.W)
+tree2.heading("Latitude", text="Latitude", anchor=tk.W)
+tree2.heading("location", text="location", anchor=tk.W)
+tree2.heading("Count", text="Count", anchor=tk.W)
 
-
-sort_btn = tk.Button(                         # Build Sort button
-    relief = "solid",
-    master = frame_left,
-    text = "Sort",
-    width = 16
+sort_btn = tk.Button(  # Build Sort button
+    relief="solid",
+    master=frame_left,
+    text="Sort",
+    width=16
 )
-sort_btn.grid(row = 3, column = 0, padx = 5, pady = 5)
+sort_btn.grid(row=3, column=0, padx=5, pady=5)
 
-
-analysis_btn = tk.Button(                      # Build Analysis button
-    relief = "solid",
-    master = frame_left,
-    text = "Analysis",
-    width = 16
+analysis_btn = tk.Button(  # Build Analysis button
+    relief="solid",
+    master=frame_left,
+    text="Analysis",
+    width=16
 )
-analysis_btn.grid(row = 4, column = 0, padx = 5, pady = 5)
+analysis_btn.grid(row=4, column=0, padx=5, pady=5)
 
-
-map_btn = tk.Button(                           # Build Map button
-    relief = "solid",
-    master = frame_left,
-    text = "Map",
-    width = 16
+map_btn = tk.Button(  # Build Map button
+    relief="solid",
+    master=frame_left,
+    text="Map",
+    width=16
 )
-map_btn.grid(row = 5, column = 0, padx = 5, pady = 5)
+map_btn.grid(row=5, column=0, padx=5, pady=5)
 
-
-status_label = tk.Label(                        # Build status label
-    master = frame_left,
-    text = "Status:",
-    bg = "gray55"
+status_label = tk.Label(  # Build status label
+    master=frame_left,
+    text="Status:",
+    bg="gray55"
 )
-status_label.grid(row = 6, column = 0, padx = 5, sticky = "w")
+status_label.grid(row=6, column=0, padx=5, sticky="w")
 
-
-status_box = tk.Label(                           # Build the status message box
-    master = frame_left,
-    height = 2,
-    width = 16,
-    wraplength = 80,
-    relief = "solid",
-    text = "Successfully read from DB",
-    bg = "green2"
+status_box = tk.Label(  # Build the status message box
+    master=frame_left,
+    height=3,
+    width=16,
+    wraplength=85,
+    relief="solid",
+    text="Successfully read from DB",
+    bg="green2"
 )
-status_box.grid(row = 7, column = 0, padx = 5, pady = 5)
-
-
+status_box.grid(row=7, column=0, padx=5, pady=5)
 
 window.mainloop()
